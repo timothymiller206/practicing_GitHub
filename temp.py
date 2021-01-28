efc65d3406cfb94a0e51f47b1829635ee43c93f9
Timmy Miller <tm206@evansville.edu>
2021-01-27 18:20:06 -0600

    Commit message test

---
+++ temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+col1=(1,5,10)
+col2=(2,4,6)
+
+
+
+np.multiply(col1, col2)
+
+
+np.divide(col1, col2)
+
+col1=[x+1 for x in col1]
+
+
+var8=9
